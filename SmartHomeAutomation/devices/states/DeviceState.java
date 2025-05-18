1134e579af0d7c21a42fb35d815387cd1b06ff91
SumeyyeBire <148131933+SumeyyeBire@users.noreply.github.com>
2025-05-18 16:57:20 +0300

    Update DeviceState.java

--- SmartHomeAutomation/devices/states/DeviceState.java
+++ SmartHomeAutomation/devices/states/DeviceState.java
@@ -1,5 +1,31 @@
-package devices;
+package devices.states;
 
-public class DeviceState {
+import devices.Device;
 
+/**
+ * DeviceState arayüzü, bir cihazın sahip olabileceği durumları (açık, kapalı, bakımda vb.) tanımlar.
+ * State tasarım deseninin temelini oluşturur.
+ */
+public interface DeviceState {
+
+    /**
+     * Cihazı açmak için davranış tanımlar.
+     *
+     * @param device Hedef cihaz
+     */
+    void turnOn(Device device);
+
+    /**
+     * Cihazı kapatmak için davranış tanımlar.
+     *
+     * @param device Hedef cihaz
+     */
+    void turnOff(Device device);
+
+    /**
+     * Cihazın mevcut durumunun adını döner (örn. ON, OFF).
+     *
+     * @return Durum adı (String)
+     */
+    String getStateName();
 }
