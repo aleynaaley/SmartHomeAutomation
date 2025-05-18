f8c77ab88ee5e3b476fe487b13112c50d8d44457
SumeyyeBire <148131933+SumeyyeBire@users.noreply.github.com>
2025-05-18 16:55:08 +0300

    Update OnState.java

--- SmartHomeAutomation/devices/states/OnState.java
+++ SmartHomeAutomation/devices/states/OnState.java
@@ -1,5 +1,25 @@
 package devices.states;
 
-public class OnState {
+import devices.Device;
 
+/**
+ * Cihaz açık durumdayken geçerli olan davranışları tanımlar.
+ */
+public class OnState implements DeviceState {
+
+    @Override
+    public void turnOn(Device device) {
+        System.out.println(device.getName() + " zaten açık.");
+    }
+
+    @Override
+    public void turnOff(Device device) {
+        System.out.println(device.getName() + " kapatılıyor...");
+        device.setState(new OffState());
+    }
+
+    @Override
+    public String getStateName() {
+        return "ON";
+    }
 }
