6d4859938b2e0809e0905a2e486429c7a789ab97
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2025-05-18 17:52:25 +0300

    Update Main.java

---
+++ SmartHomeAutomation/Main.java
@@ -0,0 +1,38 @@
+    // Tekil SmartHomeController nesnesi alınır
+    SmartHomeController controller = SmartHomeController.getInstance();
+
+    // Cihazlar oluşturulur
+    Light livingRoomLight = new Light("Living Room Light");
+    Heater bedroomHeater = new Heater("Bedroom Heater");
+
+    // Cihazlar sisteme eklenir
+    controller.addDevice(livingRoomLight);
+    controller.addDevice(bedroomHeater);
+
+    // Tüm cihazların durumu yazdırılır (başlangıçta kapalı olmalı)
+    System.out.println("\nBaşlangıç durumu:");
+    controller.printAllDeviceStatus();
+
+    // Tüm cihazlar açılır
+    System.out.println("\nTüm cihazlar açılıyor:");
+    controller.turnOnAllDevices();
+
+    // Işığı tekrar açmayı dene (zaten açık)
+    System.out.println("\nIşık tekrar açılmaya çalışılıyor:");
+    livingRoomLight.turnOn();
+
+    // Isıtıcının sıcaklığı ayarlanır
+    bedroomHeater.setTemperature(24);
+
+    // Durum tekrar yazdırılır
+    System.out.println("\nGüncel durumlar:");
+    controller.printAllDeviceStatus();
+
+    // Tüm cihazlar kapatılır
+    System.out.println("\nTüm cihazlar kapatılıyor:");
+    controller.turnOffAllDevices();
+
+    // Son durum yazdırılır
+    System.out.println("\nFinal durum:");
+    controller.printAllDeviceStatus();
+}
