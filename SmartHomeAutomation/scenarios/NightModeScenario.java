9d1e9cfe0d032695278d8c3e058439468ca92286
Aleyna Erkul <147741440+aleynaaley@users.noreply.github.com>
2025-06-16 20:49:29 +0300

    Update NightModeScenario.java

--- SmartHomeAutomation/scenarios/NightModeScenario.java
+++ SmartHomeAutomation/scenarios/NightModeScenario.java
@@ -3,25 +3,33 @@ package scenarios;
 import controllers.SmartHomeController;
 import devices.Device;
 import devices.Light;
+import devices.DoorLockSystem;
 
 /**
  * Gece modu senaryosudur.
- * Işıkları kapatır,tüm ısıtıcılar açık kalabilir.
+ * Eve varınca yapılacaklar senaryosu üzerinden gidilmiştir.
+ * Işıkları açılır ve kilit sistemi kapatılır.
+ * Isıtıcılar ve buzdolabı açık kalabilir.
  */
+
 public class NightModeScenario extends Scenario {
 
     @Override
     protected void executeScenario() {
-        System.out.println("\nGece modu başlatıldı.");
+        System.out.println("\n Gece modu başlatıldı.");
         SmartHomeController controller = SmartHomeController.getInstance();
 
         for (Device device : controller.getDevices()) {
-            if (device instanceof Light && device.isOn()) {
+            if (device instanceof Light && !device.isOn()) {
+                device.turnOn();
+            }
+        }
+        for (Device device : controller.getDevices()) {
+            if (device instanceof DoorLockSystem && device.isOn()) {
                 device.turnOff();
             }
         }
-
-        System.out.println("Işıklar kapatıldı.\n");
-        System.out.println("İyi uykular :)\n");
+        System.out.println("Işıklar açıldı ve kilit sistemi kapatıldı.\n");
+        System.out.println("İyi akşamlar :)\n");
     }
 }
